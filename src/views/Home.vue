c28d552e43e6393d27fc4fa12ccb3d81f4f6855d
harum shidiqi <harum.shidiqi@mavila.co.id>
2019-08-12 23:01:13 +0700

    styling vuex source link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home">
     <TodoList></TodoList>
+    <div style="text-align: center; margin-top: 30px;">
+      <a
+        target="_blank"
+        style="color: white !important; text-decoration: none; margin-right: 20px;"
+        href="https://vuex.vuejs.org/"
+      >Vuex Documentations</a>
+      <a
+        target="_blank"
+        style="color: white !important; text-decoration: none;"
+        href="https://scrimba.com/g/gvuex"
+      >Vuex video tutorials</a>
+    </div>
   </div>
 </template>
 
